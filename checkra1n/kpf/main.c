b6430cca10db97565c40d3f038f49ec5ed19eb20
ねむい <42214504+dora2-iOS@users.noreply.github.com>
2023-01-12 03:53:49 +0900

    added asdfugil's rootdev_module

--- checkra1n/kpf/main.c
+++ checkra1n/kpf/main.c
@@ -2793,26 +2793,26 @@ void command_kpf() {
         if (!val) panic("invalid devicetree: no prop!");
         // get fs_type
         uint8_t* rwpatch = (uint8_t*)val;
-        //printf("old system vol.fs_type: %016llx: %c\n", (uint64_t)val, rwpatch[1]);
+        printf("old system vol.fs_type: %016llx: %c\n", (uint64_t)val, rwpatch[1]);
         // change ro -> rw
         rwpatch[1] = 'w';
-        //printf("new system vol.fs_type: %016llx: %c\n", (uint64_t)val, rwpatch[1]);
+        printf("new system vol.fs_type: %016llx: %c\n", (uint64_t)val, rwpatch[1]);
         
-        if(checkrain_option_enabled(gkpf_flags, checkrain_kpf_option_fakelaunchd)) {
-            uint32_t len = 0;
-            dt_node_t* dev = dt_find(gDeviceTree, "chosen");
-            if (!dev) panic("invalid devicetree: no device!");
-            uint32_t* val = dt_prop(dev, "root-matching", &len);
-            if (!val) panic("invalid devicetree: no prop!");
-            
-            char str[0x100]; // max size = 0x100
-            memset(&str, 0x0, 0x100);
-            sprintf(str, "<dict ID=\"0\"><key>IOProviderClass</key><string ID=\"1\">IOService</string><key>BSD Name</key><string ID=\"2\">disk1s%d</string></dict>", 8);
-            
-            memset(val, 0x0, 0x100);
-            memcpy(val, str, 0x100);
-            printf("set new entry: %016llx: disk1s%d\n", (uint64_t)val, 8);
-        }
+//        if(checkrain_option_enabled(gkpf_flags, checkrain_kpf_option_fakelaunchd)) {
+//            uint32_t len = 0;
+//            dt_node_t* dev = dt_find(gDeviceTree, "chosen");
+//            if (!dev) panic("invalid devicetree: no device!");
+//            uint32_t* val = dt_prop(dev, "root-matching", &len);
+//            if (!val) panic("invalid devicetree: no prop!");
+//
+//            char str[0x100]; // max size = 0x100
+//            memset(&str, 0x0, 0x100);
+//            sprintf(str, "<dict ID=\"0\"><key>IOProviderClass</key><string ID=\"1\">IOService</string><key>BSD Name</key><string ID=\"2\">disk1s%d</string></dict>", 8);
+//
+//            memset(val, 0x0, 0x100);
+//            memcpy(val, str, 0x100);
+//            printf("set new entry: %016llx: disk1s%d\n", (uint64_t)val, 8);
+//        }
     }
     
     if(checkrain_option_enabled(gkpf_flags, checkrain_kpf_option_fakelaunchd))
@@ -2910,6 +2910,35 @@ void overlay_cmd(const char* cmd, char* args) {
     loader_xfer_recv_count = 0;
 }
 
+// asdfugil's rootdev_module: https://github.com/asdfugil/rootdev_module
+char* set_rootdev(const char* rootdev) {
+    uint32_t len = 0;
+    dt_node_t* dev = dt_find(gDeviceTree, "chosen");
+    char str[256] = "<dict ID=\"0\"><key>IOProviderClass</key><string ID=\"1\">IOService</string><key>BSD Name</key><string ID=\"2\">";
+    char* val = (char*)dt_prop(dev, "root-matching", &len);
+    /* Only the NULL at last is counted, so 256 */
+    if (strlen(str) + strlen(rootdev) + sizeof("</string></dict>") > 256) {
+        errno = ENAMETOOLONG;
+        return NULL;
+    }
+    strcat(str, rootdev);
+    strcat(str, "</string></dict>");
+    memset((void*)val, 0x0, 256);
+    snprintf(val, 256, "%s", str);
+    return val;
+}
+
+void rootdev_command(const char* cmd, char* args) {
+    if (strlen(args) == 0) {
+        printf("%s usage: %s <new root device like disk0s1s8>\n", cmd, cmd);
+        return;
+    }
+    char* val = set_rootdev(args);
+    printf("set new entry: %016llx: %s \n", (uint64_t)val, args);
+    return;
+}
+
+
 void module_entry() {
     puts("");
     puts("");
@@ -2940,6 +2969,7 @@ void module_entry() {
     command_register("kpf", "running checkra1n-kpf without booting (use bootux afterwards)", command_kpf);
     command_register("overlay", "loads an overlay disk image", overlay_cmd);
     command_register("conversion_patch", "conversion patch", kpf_set_conversion_patch_cmd);
+    command_register("set_rootdev", "Sets root device in root-matching in the device tree", rootdev_command);
 }
 char* module_name = "checkra1n-kpf2-12.0,14.5";
 
